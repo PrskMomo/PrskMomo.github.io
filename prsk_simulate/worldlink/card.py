5e39f304370e6763b44c3d594ee487c3967b4697
PrskMomo <taikifuwa1060@gmail.com>
2025-06-28 23:44:30 +0900

    Add files via upload

---
+++ prsk_simulate/worldlink/card.py
@@ -0,0 +1,125 @@
+import os
+import json
+import customtkinter
+
+FONT_TYPE = "meiryo"
+
+TYPE = ["Cute", "Cool", "Pure", "Happy", "Mysterious"]
+
+dict = {
+    "Virtual": ["Miku", "Rin", "Len", "Luka", "Meiko", "Kaito"],
+    "Leo": ["Ichika", "Saki", "Honami", "Shiho"],
+    "Mmj": ["Minori", "Haruka", "Airi", "Shizuku"],
+    "Vivid": ["Kohane", "An", "Akito", "Toya"],
+    "Wonder": ["Tsukasa", "Emu", "Nene", "Rui"],
+    "25": ["Kanade", "Mafuyu", "Ena", "Mizuki"]
+}
+
+data = {}
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # メンバー変数の設定
+        self.fonts = (FONT_TYPE, 15)
+        # フォームサイズ設定
+        self.geometry("400x220")
+        self.title("Project SEKAI Card Registry")
+
+        # フォームのセットアップをする
+        self.setup_form()
+        
+    
+    def setup_form(self):
+        def selected(event):
+            name_box.configure(values=dict[group_box.get()])
+            name_box.set(dict[group_box.get()][0])
+
+        def submit():
+            with open("member_db.json", mode="a+", encoding="utf-8") as f:
+                data[card_id_box.get()] = {}
+                data[card_id_box.get()]["Name"] = name_box.get()
+                data[card_id_box.get()]["Group"] = group_box.get()
+                data[card_id_box.get()]["Type"] = type_box.get()
+                data[card_id_box.get()]["Rarity"] = rare.get()
+                data[card_id_box.get()]["Performance"] = perform_box.get()
+                data[card_id_box.get()]["ImageLink"] = f'../images/cards/{group_box.get().lower()}/{name_box.get().lower()}/{image_box.get()}.png'
+                
+                f.write(json.dumps(data))
+
+
+        var = customtkinter.StringVar()
+        # CustomTkinter のフォームデザイン設定
+        customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+        customtkinter.set_default_color_theme("blue")
+
+        card_id = customtkinter.CTkLabel(master=self, text="Card ID")
+        card_id_box = customtkinter.CTkEntry(master=self, width=100, justify=customtkinter.CENTER)
+
+        type = customtkinter.CTkLabel(master=self, text="Type")
+        type_box = customtkinter.CTkComboBox(master=self, values=TYPE, width=120)
+        
+        group = customtkinter.CTkLabel(master=self, text="Group")
+        group_box = customtkinter.CTkComboBox(master=self, values=list(dict.keys()), command=selected, width=100)
+        
+        name = customtkinter.CTkLabel(master=self, text="Name")
+        name_box = customtkinter.CTkComboBox(master=self, values=dict[group_box.get()], width=120)
+
+        rare = customtkinter.IntVar()
+        rare.set(1)
+
+        rare1 = customtkinter.CTkRadioButton(master=self, text="★１", variable=rare, value=1)
+        rare2 = customtkinter.CTkRadioButton(master=self, text="★２", variable=rare, value=2)
+        rare3 = customtkinter.CTkRadioButton(master=self, text="★３", variable=rare, value=3)
+        rare4 = customtkinter.CTkRadioButton(master=self, text="★４", variable=rare, value=4)
+        rare5 = customtkinter.CTkRadioButton(master=self, text="Birth Day", variable=rare, value=5)
+
+        perform = customtkinter.CTkLabel(master=self, text="Performance")
+        perform_box = customtkinter.CTkEntry(master=self, width=100, justify=customtkinter.CENTER)
+
+        image = customtkinter.CTkLabel(master=self, text="Image")
+        image_box = customtkinter.CTkEntry(master=self, width=100, justify=customtkinter.CENTER)
+
+        btn = customtkinter.CTkButton(master=self, text="Submit", width=75, command=submit)
+
+
+        card_id.place(x=20, y=20)
+        card_id_box.place(x=80, y=20)
+
+        type.place(x=200, y=20)
+        type_box.place(x=250, y=20)
+
+        group.place(x=20, y=60)
+        group_box.place(x=80, y=60)
+
+        name.place(x=200, y=60)
+        name_box.place(x=250, y=60)
+
+        rare1.place(x=20, y=105)
+        rare2.place(x=90, y=105)
+        rare3.place(x=160, y=105)
+        rare4.place(x=230, y=105)
+        rare5.place(x=300, y=105)
+
+        perform.place(x=20, y=140)
+        perform_box.place(x=110, y=140)
+
+        image.place(x=225, y=140)
+        image_box.place(x=270, y=140)
+
+        btn.place(x=293, y=180)
+    
+        
+
+
+
+    
+    
+    
+
+
+if __name__ == "__main__":
+    # アプリケーション実行
+    app = App()
+    app.mainloop()
---
+++ prsk_simulate/worldlink/script.js
@@ -0,0 +1,254 @@
+const CARDS = {
+    "1": {
+        "Name": "Ichika",
+        "Group": "Leo",
+        "Type": "Cool",
+        "Rarity": 1,
+        "Performance": 8475,
+        "ImageLink": "../images/cards/leo/ichika/1.png"
+    },
+    "2": {
+        "Name": "Ichika",
+        "Group": "Leo",
+        "Type": "Happy",
+        "Rarity": 2,
+        "Performance": 18325,
+        "ImageLink": "../images/cards/leo/ichika/2.png"
+    },
+    "3": {
+        "Name": "Ichika",
+        "Group": "Leo",
+        "Type": "Mysterious",
+        "Rarity": 3,
+        "Performance": 30275,
+        "ImageLink": "../images/cards/leo/ichika/3.png"
+    },
+    "4": {
+        "Name": "Ichika",
+        "Group": "Leo",
+        "Type": "Cute",
+        "Rarity": 4,
+        "Performance": 34295,
+        "ImageLink": "../images/cards/leo/ichika/4.png"
+    }
+}
+
+window.onload = function() {
+
+    let MEMBER = ["first", "second", "third", "fourth", "fifth"];
+    let WAKU = [["first", 0], ["second", 0], ["third", 0], ["fourth", 0], ["fifth", 0]];
+    let performance = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]; //[perform, master, canvas]
+    
+    let FOCUS = [""];
+    
+    WAKU.forEach(function(element){
+        document.getElementById(`member_${element[0]}`).addEventListener("click", function (){ focus(element); });
+        document.getElementById(`master_${element[0]}`).addEventListener("click", function (e){ setMaster(e, element); });
+        document.getElementById(`skill_${element[0]}`).addEventListener("click", function (e){ setSkill(e, element);});
+        document.getElementById(`canvas_${element[0]}`).addEventListener("click", function (e){ setCanvas(e, element); }); 
+    
+  
+    });
+
+    
+    
+    setCard(CARDS);
+
+    function calcPerformAll(){
+        let total = 0;
+        let All = [];
+        for(let i = 0; i < 5; i++){
+            total = performance[i].reduce((s, v) => s + v, 0);
+            document.getElementById(`perform_num_${MEMBER[i]}`).textContent = total;
+
+            All.push(parseInt(document.getElementsByClassName("perform_num")[i+1].textContent));
+        }
+
+        document.getElementById(`perform_num_all`).textContent = All.reduce((s, v) => s + v, 0);
+    }
+
+    function focus(element){
+        FOCUS[0] = element[0];
+
+        for(let i = 0; i <= 4; i++){
+            document.getElementById(`member_${WAKU[i][0]}`).style.setProperty("--focus", "0");
+
+            if(FOCUS[0] == WAKU[i][0]){
+                document.getElementById(`member_${FOCUS[0]}`).style.setProperty("--focus", "2px solid black");
+            }
+        }
+    }
+
+    function setImage(waku, imageLink, perform, master, canvas){
+        document.getElementById(`member_${waku}`).style.backgroundImage = `url(${imageLink})`;
+        document.getElementById(`perform_num_${waku}`).textContent = perform;
+
+        performance[MEMBER.indexOf(waku[0])][0] = perform;
+        performance[MEMBER.indexOf(waku[0])][1] = master;
+        performance[MEMBER.indexOf(waku[0])][2] = canvas;
+
+        
+        /*if(trained){
+            if(!element[1]){
+                element[1] = 1;
+                
+            } else {
+                element[1] = 0;
+                document.getElementById(`member_${waku}`).style.backgroundImage = "url('https://static.wikitide.net/projectsekaiwiki/0/09/Airi_4_trained_thumbnail.png')"
+                document.getElementById(`perform_num_${waku}`).textContent = TEST[0][1]
+            }
+        } else {*/
+         
+        calcPerformAll();
+    }
+
+    function setSkill(event, element){
+        let lv = document.getElementById(`skill_${element[0]}`).textContent;
+        focus(element);
+        
+        switch(lv){
+            case "SLv.1":
+                document.getElementById(`skill_${element[0]}`).textContent = "SLv.2";
+                break;
+            
+            case "SLv.2":
+                document.getElementById(`skill_${element[0]}`).textContent = "SLv.3";
+                break;
+
+            case "SLv.3":
+                document.getElementById(`skill_${element[0]}`).textContent = "SLv.4";
+                break;
+
+            case "SLv.4":
+                document.getElementById(`skill_${element[0]}`).textContent = "SLv.1";
+                break;
+        }
+
+        event.stopPropagation();
+    }
+
+    function setMaster(event, element){
+        let master = document.getElementById(`master_${element[0]}`).textContent;
+        focus(element);
+        
+        switch(master){
+            case "0":
+                document.getElementById(`master_${element[0]}`).textContent = "1";
+                document.getElementById(`master_${element[0]}`).style.opacity = 100;
+                break;
+            
+            case "1":
+                document.getElementById(`master_${element[0]}`).textContent = "2";
+                document.getElementById(`master_${element[0]}`).style.backgroundPosition = "25.1%"
+                break;
+            
+            case "2":
+                document.getElementById(`master_${element[0]}`).textContent = "3";
+                document.getElementById(`master_${element[0]}`).style.backgroundPosition = "50.2%";
+                break;
+            
+            case "3":
+                document.getElementById(`master_${element[0]}`).textContent = "4";
+                document.getElementById(`master_${element[0]}`).style.backgroundPosition = "75.3%";
+                break;
+            
+            case "4":
+                document.getElementById(`master_${element[0]}`).textContent = "5";
+                document.getElementById(`master_${element[0]}`).style.backgroundPosition = "100.4%";
+                break;
+            
+            case "5":
+                document.getElementById(`master_${element[0]}`).textContent = "0";
+                document.getElementById(`master_${element[0]}`).style.opacity = 0;
+                document.getElementById(`master_${element[0]}`).style.backgroundPosition = "0%";
+                break;
+        }
+
+        let RANK = parseInt(document.getElementById(`master_${element[0]}`).textContent);
+        let Index = MEMBER.indexOf(element[0]);
+        performance[Index][1] = MasterRank(WAKU[Index][1], RANK);
+        calcPerformAll();
+        event.stopPropagation();
+    }
+
+    function setCanvas(event, element){
+        focus(element);
+
+        if(!element[2]){
+            element[2] = 1;
+            document.getElementById(`canvas_${element[0]}`).style.backgroundImage = "url('../images/canvas_yes.png')"
+            document.getElementById(`canvas_${element[0]}`).textContent = 1
+        } else {
+            element[2] = 0;
+            document.getElementById(`canvas_${element[0]}`).style.backgroundImage = "url('../images/canvas_no.png')"
+            document.getElementById(`canvas_${element[0]}`).textContent = 0
+        }
+        
+        let RANK = parseInt(document.getElementById(`canvas_${element[0]}`).textContent);
+        let Index = MEMBER.indexOf(element[0]);
+        performance[Index][2] = CanvasRank(WAKU[Index][1], RANK);
+        calcPerformAll();
+        event.stopPropagation();
+    }
+
+    /*fetch("./member_db.json")
+            .then((data) => data.text())
+            .then((res) => console.log(Object.keys(res).length));
+        */
+    
+    function MasterRank(rare, rank){
+        let mul = 0;
+        switch(rare){
+            case 1: mul = rank * 150; break;
+            case 2: mul = rank * 300; break;
+            case 3: mul = rank * 450; break;
+            case 4: mul = rank * 600; break;
+            case 5: mul = rank * 540; break;
+        }
+
+        return mul;
+    }
+
+    function CanvasRank(rare, canvas){
+        let mul = 0;
+        if(canvas){
+            switch(rare){
+                case 1: mul = 300; break;
+                case 2: mul = 600; break;
+                case 3: mul = 900; break;
+                case 4: mul = 1500; break;
+                case 5: mul = 1200; break;
+            }
+        }
+
+        return mul;
+    }
+    
+    function setCard(json){
+        let cards = document.getElementById('cards');
+        for(let i = 1; i <= Object.keys(json).length; i++){
+            let new_card = document.createElement('div');
+            new_card.className = "card";
+            new_card.id = `card${i}`;
+            new_card.textContent = `${json[i].Rarity};${json[i].Performance}`;
+            new_card.style.backgroundImage = `url(${json[i].ImageLink})`;
+            
+
+            cards.appendChild(new_card);
+        }
+
+        for(let c = 1; c <= document.getElementsByClassName("card").length; c++){
+            document.getElementsByClassName("card")[c-1].addEventListener("click", () => {
+                let rare = json[c].Rarity;
+                let Master = document.getElementById(`master_${FOCUS}`).textContent;
+                setImage(FOCUS, json[c].ImageLink, json[c].Performance, MasterRank(rare, Master), CanvasRank(rare, WAKU[MEMBER.indexOf(FOCUS[0])][2]));
+                WAKU[MEMBER.indexOf(FOCUS[0])][1] = json[c].Rarity;
+                
+            });
+      
+        }
+        
+        calcPerformAll();
+    }
+          
+}
